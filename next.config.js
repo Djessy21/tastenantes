0856cfa7ce5b84426554c94b292784a1091bebb4
ATTA <djessyatta@live.fr>
2025-03-05 21:54:56 +0100

    fix: correction de la configuration pour le déploiement Vercel

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     domains: ["maps.googleapis.com"],
   },
+  output: "standalone",
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
